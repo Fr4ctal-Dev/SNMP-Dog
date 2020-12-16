189f499c049a675508867d847aa9cb5b54c99255
Fr4ctal-Dev <plankermatteo23@gmail.com>
2020-12-16 10:24:44 +0100

    Add table operations and row operations

--- backend/snmp.py
+++ backend/snmp.py
@@ -3,6 +3,49 @@ import ipaddress
 from pysnmp.hlapi import *
 
 
+def get_device(ip):
+    values = ("sysDescr", "sysUpTime", "sysLocation", "sysName")
+    device_info = {}
+    device_info["ip"] = ip
+    for value in values:
+        iterator = getCmd(SnmpEngine(),
+                          CommunityData('public'),
+                          UdpTransportTarget((str(ip), 161), timeout=0.1, retries=0),
+                          ContextData(),
+                          ObjectType(ObjectIdentity('SNMPv2-MIB', str(value), 0)))
+
+        error_indication, error_status, error_index, var_binds = next(iterator)
+
+        if error_indication:  # SNMP engine errors
+            print(str(ip) + ">  " + str(error_indication))
+        else:
+            if error_status:  # SNMP agent errors
+                print(
+                    '%s at %s' % (
+                        error_status.prettyPrint(), var_binds[int(error_index) - 1] if error_index else '?'))
+            else:
+                for varBind in var_binds:  # SNMP response contents
+                    device_info[value] = str(varBind).split("=")[1].replace(" ", "")
+
+    try:
+        # Add more OSs here
+        desc = str(device_info["sysDescr"]).lower()
+        if "router" in desc:
+            device_info["sysDescr"] = "Mikrotik RouterOS"
+        elif "windows" in desc:
+            device_info["sysDescr"] = "Windows"
+        elif "mac" in desc:
+            device_info["sysDescr"] = "macOS"
+        elif "linux" in desc:
+            device_info["sysDescr"] = "Linux"
+        else:
+            device_info["sysDescr"] = "Other/Unknown"
+    except KeyError:
+        pass
+
+    return device_info
+
+
 def get_devices(ip, mask):
     """
     Get all device info from specified network
@@ -61,7 +104,7 @@ def get_iterator(ip, value, community_string, is_oid=False):
             ip: The device's IP address
             value: MIB id or SNMP OID
             community_string: The community string for the network. Usually 'public' or 'private'
-            is_oid: boolean if the provided #value is a OID or a MIB ID
+            is_oid: boolean if the provided value is a OID or a MIB ID
 
         Returns:
             The usable iterator
@@ -137,5 +180,3 @@ def get_device_value_oid(ip, oid, community_string="public"):
         else:
             for varBind in var_binds:  # SNMP response contents
                 return str(varBind).split("=")[1].replace(" ", "")
-
-
